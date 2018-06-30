6b934ff37ba9ac39ae9e50e8c91b63e7e21dab47
Jennifer Snyder <jlsnyder09@gmail.com>
2018-06-30 19:05:14 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,5 @@
 require "pry"
-animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"
+animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia" }
 
 class Hash
   def keys_of(arguments)
