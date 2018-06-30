de041f9dbb63fae69fb735d7c688bf7db6933912
Jennifer Snyder <jlsnyder09@gmail.com>
2018-06-30 19:27:18 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -5,7 +5,7 @@ class Hash
   def keys_of(*arguments)
     keys = []
     each do |key, value|
-      if value == arguments
+      if arguments.include?(value)
         keys << key
       end
     end
