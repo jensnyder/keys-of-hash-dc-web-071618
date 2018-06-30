a7e09b65eaf97d3222672e5548788a66fc0c1a2f
Jennifer Snyder <jlsnyder09@gmail.com>
2018-06-30 19:26:22 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,9 +3,13 @@ animals = {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tort
 
 class Hash
   def keys_of(*arguments)
+    keys = []
     each do |key, value|
+      if value == arguments
+        keys << key
     end
   end
+  keys
 end
 
 puts animals.keys_of("Panama")
